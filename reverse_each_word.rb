c10cd624eff5db033d0440f4d7d560a00547b8f5
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-22 04:45:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-def reverse_each_word(sentence2)
-   setence = ["Hello there, how are you?"]
-   puts sentence.reverse
+def reverse_each_word
+   sentence = ["Hello there, how are you?"]
+   sentence.reverse
 end
